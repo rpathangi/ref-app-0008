c1bfb0cfddd3673e2415e459e97f6adf3cfd1895
rpathangi <rpathangi@gmail.com>
2017-08-08 00:05:00 -0700

    Checking in missing files

---
+++ src/main/java/com/rpathangi/spring/Application.java
@@ -0,0 +1,17 @@
+package com.rpathangi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+}
