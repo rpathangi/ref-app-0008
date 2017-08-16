b7e6c9716bd069a172ed6129aa6781bce72e0de4
rpathangi <rpathangi@gmail.com>
2017-08-15 23:54:36 -0700

    20170815 - Read Properties from an external file.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ref-app-0008
 
+# Features
+20170815 - Read properties from an external file.
+20170813 - Integrate with GoogleAppEngine.
+20170810 - Expose a basic rest endpoint using spring boot.
+
 
 
 # Issue # 1
--- src/main/java/com/rpathangi/spring/HelloWorldService.java
+++ src/main/java/com/rpathangi/spring/HelloWorldService.java
@@ -1,16 +1,26 @@
 package com.rpathangi.spring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
+@ComponentScan(basePackages = { "com.rpathangi.spring" })
+@PropertySource("classpath:application.properties")
 @RequestMapping("/sayHello")
 @RestController
 public class HelloWorldService {
 
+    @Value("${greeting}")
+    private String greeting;
+
     @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public HelloWorldResponse sayHello() {
-        return new HelloWorldResponse("Hello World!");
+        return new HelloWorldResponse(greeting);
     }
 }
