abe0261ede4f64cf926d7558f74278edcf7b2ead
rpathangi <rpathangi@gmail.com>
2017-08-30 22:48:24 -0700

    20170830 - Integrate property management and external batch scheduler into docker.

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
+tiller -b $PIO_HOME/conf -e $ENVIRONMENT_NAME -v
+
+source /app/pio/conf/pio_config_tiller.sh
+
 pio-start-all
 
 jps -l
 
 pio status
+
+cd /app/ndscheduler
+
+make simple >> /logs/"$(date +"ndscheduler_%Y%m%d%H%M%S").log"
