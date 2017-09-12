e97d5e28bd182da930cf04e3c99f8451fb57a95b
rpathangi <rpathangi@gmail.com>
2017-09-11 22:40:10 -0700

    20170911 - Integrate with MyRecommendation Template

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -15,3 +15,19 @@ pio status
 cd /app/ndscheduler
 
 nohup make simple >> /logs/"$(date +"ndscheduler_%Y%m%d%H%M%S").log" &
+
+cd $PIO_HOME/MyRecommendation
+
+pio app new MyApp1
+
+export ACCESS_KEY=$(pio app list | awk '{ print $7 }' | sed '2q;d')
+
+curl https://raw.githubusercontent.com/apache/spark/master/data/mllib/sample_movielens_data.txt --create-dirs -o data/sample_movielens_data.txt
+
+python data/import_eventserver.py --access_key $ACCESS_KEY
+
+pio build --verbose
+
+pio train
+
+pio deploy
